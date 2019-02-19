8d07ec3c4142db30185ea91b672610f2a183d6a6
Shenbagam Shanmugam <ce1040@ce-1040.cssad.compassitesinc.com>
2019-02-19 12:11:02 +0530

    login page started

---
+++ login/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <loginpage></loginpage>
+  </div>
+</template>
+
+<script>
+import loginpage from './components/loginpage.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+   loginpage 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
