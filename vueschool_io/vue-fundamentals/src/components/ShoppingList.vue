8e206b2d8466966c4ec8cb4c90df54f38c41a65f
Shenbagam Shanmugam <ce1040@ce-1040-1.cssad.compassitesinc.com>
2020-01-02 12:01:49 +0530

    Initial

---
+++ vueschool_io/vue-fundamentals/src/components/ShoppingList.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="shoppinglist">
+        <p>Welcome to the shopping list vue app</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name : "shoppinglist"
+    }
+</script>
